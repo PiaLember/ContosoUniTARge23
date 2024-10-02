b1a7963b1fbe1bc047e04d807ff468e46be63342
PiaLember <pia.lember@gmail.com>
2024-10-02 09:06:22 +0300

    Final commit

---
+++ ContosoUniTARge23/Models/SchoolViewModels/InstructorIndexData.cs
@@ -0,0 +1,11 @@
+﻿using ContosoUniTARge23.Models;
+
+namespace ContosoUniTARge23.Models.SchoolViewModels
+{
+    public class InstructorIndexData
+    {
+        public IEnumerable<Instructor> Instructors { get; set; }
+        public IEnumerable<Course> Courses { get; set; }
+        public IEnumerable<Enrollment> Enrollments { get; set; }
+    }
+}
